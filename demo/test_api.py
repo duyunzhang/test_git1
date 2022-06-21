c5e7bdd6a0296acc1e012feaf653046f3e340acb
duyunzhang <1572324532@qq.com>
2022-06-21 12:13:05 +0800

    快递测试接口

---
+++ demo/test_api.py
@@ -0,0 +1,23 @@
+'''
+@file :  test_api.py
+@author : 张福卫
+@date : 2022/6/21 12:09
+'''
+
+
+import requests
+import json
+
+
+import requests
+import json
+from test_data import *
+
+url = 'http://www.kuaidi100.com/query'
+params = {'type': 中通,
+          'postid': '75527538262087'
+
+          }
+res = requests.get(url=url, params=params)
+m = json.dumps(res.json(), ensure_ascii=False, sort_keys=True, indent=4, separators=(', ', ': '))
+print(m)
---
+++ demo/test_data.py
@@ -0,0 +1,7 @@
+'''
+@file :  test_data.py
+@author : 张福卫
+@date : 2022/6/21 12:11
+'''
+
+中通='zhongtong'
