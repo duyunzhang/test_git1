dfd1ad5ffc65f98dbaeeb53837586b7c6c45211b
duyunzhang <1572324532@qq.com>
2022-07-06 20:05:33 +0800

    数据库代码

--- demo2/test_data.py
+++ demo2/test_data.py
@@ -3,8 +3,9 @@
 @author : 张福卫
 @date : 2022/6/27 22:11
 '''
-
 from locall import *
+from xingming.test_xm import *
+
 import random
 
 def id_select(user_id):
@@ -43,10 +44,12 @@ def all_select(sql):
     # 提交
     conn.commit()
     # 打印结果
-    results = cursor.fetchall()
+    res = cursor.fetchall()
+    print('查询返回数据共' + '【' + str(len(res)) + '】' + '条')
     print("表头:\n", "  ".join([item[0] for item in cursor.description]))
 
-    for row in results:
+
+    for row in res:
         print(row)
         # print(row[1])
 
@@ -87,12 +90,16 @@ def inser_sql(sql):
 
 def test_updata():
     '''批量操作数据库 '''
+
     conn = localhost
     # 创建游标
     cursor = conn.cursor()
-    for i in range(22,40):
-        m=random.randrange(1,200)
-        s1 = f"UPDATE `test_1`.`a` SET `name` = '{'张三'+str(m)}', `age` = 18 WHERE `id` = {i} "
+    for i in range(1,1000):
+        m=random.randrange(18,22)
+        # tom = XM1()
+        # print(tom)
+        # s1 = f"UPDATE `test_1`.`a` SET `name` = '{tom}', `age` = 18 WHERE `id` = {i} " #修改姓名
+        s1 = f"UPDATE `test_1`.`a` SET `age` = '{str(m)}' WHERE `id` = {i} "  #修改年龄
         # s1 = f"UPDATE finance_order SET order_status = 3  where username = '赵峰' and product_id = 2022032410164444233"
         # s1 = "UPDATE finance_order SET buy_time = '%s' where username = '杨晓宇' and product_id = 2022032410164444233"%(today)
         print(s1)
--- demo2/test_run.py
+++ demo2/test_run.py
@@ -4,9 +4,23 @@
 @date : 2022/6/27 23:45
 '''
 
-sql='select * from a  order by id desc'
+from  xingming.test_xm import *
+import random
+
+sql='select distinct(name) from a  order by id desc'
+sql5='select * from a group by age  having count(age)>1'
+sql1='select * from a where name in (select name from a group by name having count(name)>1)' #查询姓名相同的所有记录
+sql4='select * from a where age in (select age from a group by age having count(age)>1)'  #年龄重复的所有记录
+sql3='select * from a where (name,age) in (select name,age from a group by name,age having count(name)>1)'
+sql2='select * from a group by name having count(name)>1'
 from test_data import *
 if __name__ == '__main__':
     # id_select(11)  #根据id查询
-    all_select(sql)  #查询所有信息
-    # inser_sql("INSERT INTO `test_1`.`a` ( `name`, `age`) VALUES ( '张三', 18)")  #添加数据
+    # all_select(sql5)  #查询所有信息
+    # all_select(sql4)  #查询所有信息
+    test_updata()
+    # for i in range(1,1000):
+    #     # tom = XM1()
+    #     m = random.randrange(1,10)
+    #     s1 = f"UPDATE `test_1`.`a` SET `calss` = '{m}' WHERE `id` = {i} "
+    #     inser_sql(s1)  #添加数据
